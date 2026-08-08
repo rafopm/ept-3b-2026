b53c9f49b7075c04357f79219ec42414cb32e372
Rafael Pampavilca <rafopm@gmail.com>
2026-08-08 02:09:53 -0500

    update script exec

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CONFIG PARA GITHUB PAGES + GOOGLE LOGIN
 // 1. URL de tu Apps Script
-const API_URL = "https://script.google.com/macros/s/TU_DEPLOYMENT_ID/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyqOJy_li5XIo5FljzKtAPRnnHw0WHPqfv5oerw0uzExgoSZbA5haLQOsKUpb5Plv8d/exec";
 
 // 2. Client ID de Google Cloud Console (OAuth 2.0 Web)
 const GOOGLE_CLIENT_ID = "789057548601-l0qujt2ebv0n151t62timi5c6vnt3fhv.apps.googleusercontent.com";
