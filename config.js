119e0091df5bc49c8a06566856a0e244bbd4bf52
Rafael Pampavilca <rafopm@gmail.com>
2026-08-08 15:30:32 -0500

    make responsive

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const API_URL = "https://lively-sound-ad87.rpampavilca.workers.dev";
 // Si cambias de Worker, cambia aqui. Si usas directo (cuando es Gmail personal):
 // const API_URL = "https://script.google.com/macros/s/TU_ID/exec";
 
-const GOOGLE_CLIENT_ID = "789057548601-l0qujt2ebv0n151t62timi5c6vnt3fhv.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "PON_AQUI_TU_CLIENT_ID.apps.googleusercontent.com";
 const ALLOWED_DOMAIN = "@colegiosanmartin.edu.pe";
