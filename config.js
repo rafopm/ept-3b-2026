d9e211f97f9f2266abb32389df4dc904c1131ada
Rafael Pampavilca <rafopm@gmail.com>
2026-08-08 03:28:04 -0500

    error en fetch exec renew

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // CONFIG GITHUB PAGES + GOOGLE LOGIN AUTO
-const API_URL = "https://script.google.com/macros/s/AKfycbzNZrSk4veYojrDvS033VwrglkH7C01kGE_V-2osWaWxUZYeEhLsJrcrk4TgFyqS9-n/exec"; 
+// const API_URL = "https://script.google.com/macros/s/AKfycbzNZrSk4veYojrDvS033VwrglkH7C01kGE_V-2osWaWxUZYeEhLsJrcrk4TgFyqS9-n/exec"; 
 // Si usas proxy Cloudflare para evitar CORS del cole, pon aqui la URL del worker:
-// const API_URL = "https://ept-proxy.tu-usuario.workers.dev";
+const API_URL = "https://lively-sound-ad87.rpampavilca.workers.dev/";
 
 const GOOGLE_CLIENT_ID = "789057548601-l0qujt2ebv0n151t62timi5c6vnt3fhv.apps.googleusercontent.com";
 const ALLOWED_DOMAIN = "@colegiosanmartin.edu.pe";
