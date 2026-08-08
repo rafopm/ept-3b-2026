8ab09ecbb570278c26ae1947d84816c7d2ea4fb2
Rafael Pampavilca <rafopm@gmail.com>
2026-08-08 02:28:44 -0500

    error en fetch exec renew

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // CONFIG PARA GITHUB PAGES + GOOGLE LOGIN
 // 1. URL de tu Apps Script
-const API_URL = "https://script.google.com/macros/s/AKfycbyba1tjaX5GkTYTFqFJdYX9VyArcEIlN8Mf1GwIHNPRVii8TogEnB4otyrgc76AbLVk/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwd-EhCph7IVnW4AvGPjDTmCdU6BHDtnvQ4w-OToZlBdpQuUx7hiQdGAiPouHkF4YsW/exec";
 
 // 2. Client ID de Google Cloud Console (OAuth 2.0 Web)
 const GOOGLE_CLIENT_ID = "789057548601-l0qujt2ebv0n151t62timi5c6vnt3fhv.apps.googleusercontent.com";
