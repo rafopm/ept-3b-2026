a88fc11d65fad8474d4c416d3d32e7a7141d0a7a
Rafael Pampavilca <rafopm@gmail.com>
2026-08-08 03:24:55 -0500

    error en fetch exec renew

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
-// CONFIG PARA GITHUB PAGES + GOOGLE LOGIN
-// 1. URL de tu Apps Script
-//const API_URL = "https://script.google.com/a/macros/colegiosanmartin.edu.pe/s/AKfycbwd-EhCph7IVnW4AvGPjDTmCdU6BHDtnvQ4w-OToZlBdpQuUx7hiQdGAiPouHkF4YsW/exec";
-const API_URL = "https://lively-sound-ad87.rpampavilca.workers.dev/";
+// CONFIG GITHUB PAGES + GOOGLE LOGIN AUTO
+const API_URL = "https://script.google.com/macros/s/AKfycbzNZrSk4veYojrDvS033VwrglkH7C01kGE_V-2osWaWxUZYeEhLsJrcrk4TgFyqS9-n/exec"; 
+// Si usas proxy Cloudflare para evitar CORS del cole, pon aqui la URL del worker:
+// const API_URL = "https://ept-proxy.tu-usuario.workers.dev";
 
-// 2. Client ID de Google Cloud Console (OAuth 2.0 Web)
 const GOOGLE_CLIENT_ID = "789057548601-l0qujt2ebv0n151t62timi5c6vnt3fhv.apps.googleusercontent.com";
-
-// 3. Dominio permitido
 const ALLOWED_DOMAIN = "@colegiosanmartin.edu.pe";
+
+
+
+
+
 
